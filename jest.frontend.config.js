bcbcc23f87a93e7ac80db161b43613b9f8b670fe
p-xp <panxinping10@gmail.com>
2026-02-17 17:58:11 +0800

    Add tests for About page

--- jest.frontend.config.js
+++ jest.frontend.config.js
@@ -27,7 +27,7 @@ export default {
 
 	// jest code coverage
 	collectCoverage: true,
-	collectCoverageFrom: ["client/src/pages/Auth/**"],
+	collectCoverageFrom: ["client/src/pages/**/*.{js,jsx}"],
 	coverageThreshold: {
 		global: {
 			lines: 100,
